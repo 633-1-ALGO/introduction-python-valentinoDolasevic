94f604acfe766d4f86a4b47f6de204c36a223e70
valentinoDolasevic <valentino.dolasevic@etu.hesge.ch>
2019-09-17 23:12:50 +0200

    Exercices Terminer 11.2

--- 11.2- Exercice - Boucle/boucle2.py
+++ 11.2- Exercice - Boucle/boucle2.py
@@ -3,3 +3,14 @@
 # Résultat attendu : Le tableau B trié
 
 B = [2, 6, 8, 5, 4, 12, 98, 34, 1]
+
+for j in range (1 , len(B)) :
+    key = B[j]
+    i = j - 1
+    while i >= 0 and B[i] > key:
+        B[i+1] = B[i]
+        i = i - 1
+    B[i+1] = key
+print (B)
+
+
