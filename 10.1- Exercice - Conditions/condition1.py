f675247d632f1eb031f452e922c0bfddec5ac56e
valentinoDolasevic <valentino.dolasevic@etu.hesge.ch>
2019-09-18 20:05:52 +0200

    modification exercice 10.1 après relecture consigne

--- 10.1- Exercice - Conditions/condition1.py
+++ 10.1- Exercice - Conditions/condition1.py
@@ -3,11 +3,11 @@
 # Résultat attendu : Un message affichant "Produit positif" ou "Produit négatif" ou "Produit nul".
 # Indications :  Vous pouvez changer les valeurs des variables pour vos tests.
 c = 42
-d = 31
+d = 0
 
-if c * d > 0:
+if c  > 0 and d > 0:
     print("Produit positif")
-elif c * d < 0:
+elif (c > 0 and d < 0) or (d > 0 and c < 0):
     print("Produit négatif")
 else:
     print("Produit nul")
